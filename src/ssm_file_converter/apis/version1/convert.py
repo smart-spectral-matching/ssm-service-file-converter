93d177e4d93f40071275e6b30dc942f7ecff209a
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-05-23 17:46:29 -0400

    Adding /json for rruff, jcamp but converts to SciData JSON-LD

--- src/ssm_file_converter/apis/version1/convert.py
+++ src/ssm_file_converter/apis/version1/convert.py
@@ -1,15 +1,47 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, HTTPException
+import os
+import pathlib
+import scidatalib.io.rruff
+import time
+
+from ssm_file_converter.utils.file_formats import (
+    INPUT_FILE_FORMAT_TYPES,
+    OUTPUT_FILE_FORMAT_TYPES,
+)
 
 convert_router = APIRouter()
 
 
 @convert_router.get("/")
 async def format_info():
-    input_formats = ["rruff", "jcamp"]
-    output_formats = ["json"]
-    return {"input formats": input_formats, "output formats": output_formats}
+    response = {
+        "input formats": INPUT_FILE_FORMAT_TYPES,
+        "output formats": OUTPUT_FILE_FORMAT_TYPES
+    }
+    return response
 
 
 @convert_router.post("/json")
 async def convert_file_to_json(file: UploadFile):
-    return {"filename": file.filename}
+    # Read upload file to local file (required for scidatalib)
+    filename = f'/tmp/{time.time()}-{file.filename}'
+    with open(f'{filename}', 'wb') as f:
+        content = await file.read()
+        f.write(content)
+
+    # Convert from file type to SciData JSON-LD
+    scidata = None
+    file_extension = pathlib.Path(filename).suffix
+    if file_extension == ".rruff":
+        scidata = scidatalib.io.rruff.read_rruff(filename)
+    if file_extension in [".jdx", ".jcamp"]:
+        scidata = scidatalib.io.jcamp.read_jcamp(filename)
+
+    # Error handling if no file extension found
+    if not scidata:
+        raise HTTPException(
+            status_code=400,
+            detail="File format not supported"
+        )
+
+    return scidata.output
