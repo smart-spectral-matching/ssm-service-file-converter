8a85a7ab23d7789ff53183725ec8369c729e4ae2
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-05-23 17:48:24 -0400

    Adds tests for /jsonld + jcamp test data

--- tests/apis/version1/test_convert.py
+++ tests/apis/version1/test_convert.py
@@ -9,13 +9,24 @@ client = TestClient(app)
 
 
 @pytest.fixture
-def raman_soddyite_file():
+def raman_soddyite_rruff_file():
     """
     Raman RRUFF file for Soddyite for wavelength 780 nm
     Retrieved on 1/12/2021 from:
         https://rruff.info/tmp_rruff/Soddyite__R060361__Broad_Scan__780__0__unoriented__Raman_Data_RAW__21504.rruff  # noqa: E501
     """
     p = pathlib.Path(TEST_DATA_DIR, "rruff", "raman_soddyite.rruff")
+    return p
+
+
+@pytest.fixture
+def raman_soddyite_jcamp_file():
+    """
+    Raman RRUFF file for Soddyite for wavelength 780 nm (in JCAMP format)
+    Retrieved RRUFF file on 1/12/2021 from:
+        https://rruff.info/tmp_rruff/Soddyite__R060361__Broad_Scan__780__0__unoriented__Raman_Data_RAW__21504.rruff  # noqa: E501
+    """
+    p = pathlib.Path(TEST_DATA_DIR, "jcamp", "raman_soddyite.jdx")
     return p
 
 
@@ -28,8 +39,30 @@ def test_convert():
     }
 
 
-def test_convert_rruff_to_json(raman_soddyite_file):
-    with open(raman_soddyite_file.absolute(), 'rb') as f:
-        files = {"file": ("filename", f)}
-        response = client.post("/convert/json", files=files)
+def test_convert_rruff_to_jsonld(raman_soddyite_rruff_file):
+    with open(raman_soddyite_rruff_file.absolute(), 'rb') as f:
+        files = {"file": (raman_soddyite_rruff_file.name, f)}
+        response = client.post("/convert/jsonld", files=files)
     assert response.status_code == 200
+
+    scidata = response.json().get("@graph")
+    assert scidata.get("title") == "Soddyite"
+    assert scidata.get("uid") == "rruff:R060361"
+    assert "scidata" in scidata
+    assert "methodology" in scidata.get("scidata")
+    assert "system" in scidata.get("scidata")
+    assert "dataset" in scidata.get("scidata")
+
+
+def test_convert_jcamp_to_jsonld(raman_soddyite_jcamp_file):
+    with open(raman_soddyite_jcamp_file.absolute(), 'rb') as f:
+        files = {"file": (raman_soddyite_jcamp_file.name, f)}
+        response = client.post("/convert/jsonld", files=files)
+    assert response.status_code == 200
+
+    scidata = response.json().get("@graph")
+    assert scidata.get("title") == "Soddyite"
+    assert scidata.get("uid") == "scidata:jcamp:jsonld"
+    assert "scidata" in scidata
+    assert "methodology" in scidata.get("scidata")
+    assert "dataset" in scidata.get("scidata")
