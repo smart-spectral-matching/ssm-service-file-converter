fb3e22916703480898e9d06e2749d26289c92164
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-02 12:40:06 -0400

    Re #2 - adds scidata jsonld -> json conversion for /convert/json

--- src/ssm_file_converter/services/scidata_converter.py
+++ src/ssm_file_converter/services/scidata_converter.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 import scidatalib.io.jcamp
 import scidatalib.io.rruff
@@ -5,10 +6,20 @@ from scidatalib.scidata import SciData
 from typing import Union
 
 
+_DEFAULT_UID = "scidata:jsonld"
+
+
 def filename_to_scidata(filename: str) -> Union[SciData, None]:
     # Convert from file type to SciData JSON-LD
     scidata = None
     file_extension = pathlib.Path(filename).suffix
+
+    if file_extension == ".jsonld":
+        with open(filename, "r") as f:
+            data = json.load(f)
+            uid = data.get("@graph").get("uid", _DEFAULT_UID)
+            scidata = SciData(uid)
+            scidata.meta = data
 
     if file_extension == ".rruff":
         scidata = scidatalib.io.rruff.read_rruff(filename)
