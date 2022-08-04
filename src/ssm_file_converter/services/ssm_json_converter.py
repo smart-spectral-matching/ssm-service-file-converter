a15beb08a7f4ef6e599f2cdb74b43e757e16df14
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-04 15:58:30 -0400

    Re #3 - adds json -> json-ld service

--- src/ssm_file_converter/services/ssm_json_converter.py
+++ src/ssm_file_converter/services/ssm_json_converter.py
@@ -2,7 +2,9 @@ from scidatalib.scidata import SciData
 
 
 def scidata_to_ssm_json(scidata: SciData) -> dict:
-    # Convert from SciData JSON-LD to SSM abbreviated JSON
+    """
+    Convert from SciData JSON-LD to SSM abbreviated JSON
+    """
     output = dict()
     output["title"] = scidata.output["@graph"]["title"]
     if scidata.output["@id"]:
@@ -120,3 +122,101 @@ def scidata_to_ssm_json(scidata: SciData) -> dict:
         # TODO: need to fix that we use 'dataseries' instead of 'dataset'
 
     return output
+
+
+def ssm_json_to_scidata(ssm_json: dict) -> SciData:
+    """
+    Convert from SSM abbreviated JSON to SciData JSON-LD
+    """
+    # Construct UID for SciData document from title
+    title = ssm_json.get("title", "ssm:dataset")
+    uid = ssm_json.get("uid", f'scidata:jsonld:{title}')
+    print(ssm_json.keys())
+
+    # Setup SciData object
+    sd = SciData(uid)
+    sd.title = title
+    sd.docid = ssm_json.get("url", "")
+
+    #   methodology
+    methodology = ssm_json.get("methodology")
+
+    if methodology:
+        if "evaluationMethod" in methodology:
+            sd.evaulation(methodology.get("evaluationMethod"))
+        elif "evaluation" in methodology:
+            sd.evaulation(methodology.get("evaluation"))
+
+
+        # aspects
+        aspects = list()
+        if "aspects" in methodology:
+            aspects = methodology.get("aspects")
+            for aspect in aspects:
+                # default to make everything a "measurement"
+                aspect["@id"] = "measurement"
+                aspect["@type"] = "sdo:measurement"
+
+        # technique aspect
+        if "techniqueType" in methodology:
+            technique = methodology.get("techniqueType")
+        if "instrument" in methodology:
+            instrument = methodology.get("instrument")
+
+        if technique or instrument:
+            aspect = dict()
+            if technique:
+                aspect["techniqueType"] = technique
+            if instrument:
+                aspect["instrument"] = instrument
+
+            # default to make everything a "measurement"
+            aspect["@id"] = "measurement"
+            aspect["@type"] = "sdo:measurement"
+            aspects.append(aspect)
+
+        if aspects:
+            sd.aspects(aspects)
+
+    #   system
+    system = ssm_json.get("system", None)
+
+    if system:
+        if "facets" in system:
+            facets = system.get("facets")
+            for facet in facets:
+                aspect["@id"] = ""
+                aspect["@type"] = "facet/"
+            sd.aspects(aspects)
+
+    #   dataset
+    # TODO: need to fix that we use 'dataseries' instead of 'dataset' (add .get("dataset"))
+    dataset = ssm_json
+
+    dataseries_list = dataset.get("dataseries", None)
+    if dataseries_list:
+        for dataseries_json in dataseries_list:
+            output_dataseries_list = list()
+            for axis, dataseries in dataseries_json.items():
+                # skip the non-axis dataseries (i.e. "hasAxisType" key)
+                if "-axis" not in axis:
+                    continue
+                output_dataseries = dict()
+                output_dataseries["@id"] = "dataseries"
+
+                label = dataseries.get("label", None)
+                if label:
+                    output_dataseries["label"] = label
+
+                parameter_list = dataseries.get("parameter", None)
+                if parameter_list:
+                    output_parameter = dict()
+                    for key, parameter in parameter_list.items():
+                        output_parameter[key] = parameter
+                    output_dataseries["parameter"] = output_parameter
+
+            output_dataseries_list.append(output_dataseries)
+
+        sd.dataseries(output_dataseries_list)
+
+    return sd
