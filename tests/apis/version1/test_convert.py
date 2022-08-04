ab2e7e239c60ef2582afecdfe54ab4786051847d
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-04 16:35:30 -0400

    Fixes test for file formatting

--- tests/apis/version1/test_convert.py
+++ tests/apis/version1/test_convert.py
@@ -1,60 +1,17 @@
 from fastapi.testclient import TestClient
 import json
-import pathlib
-import pytest
 
 from ssm_file_converter import app
-from tests import TEST_DATA_DIR
 
 FILE_ARG = "upload_file"
 client = TestClient(app)
-
-
-@pytest.fixture
-def raman_soddyite_rruff_file():
-    """
-    Raman RRUFF file for Soddyite for wavelength 780 nm
-    Retrieved on 1/12/2021 from:
-        https://rruff.info/tmp_rruff/Soddyite__R060361__Broad_Scan__780__0__unoriented__Raman_Data_RAW__21504.rruff  # noqa: E501
-    """
-    p = pathlib.Path(TEST_DATA_DIR, "rruff", "raman_soddyite.rruff")
-    return p
-
-
-@pytest.fixture
-def raman_soddyite_jcamp_file():
-    """
-    Raman RRUFF file for Soddyite for wavelength 780 nm (in JCAMP format)
-    Retrieved RRUFF file on 1/12/2021 from:
-        https://rruff.info/tmp_rruff/Soddyite__R060361__Broad_Scan__780__0__unoriented__Raman_Data_RAW__21504.rruff  # noqa: E501
-    """
-    p = pathlib.Path(TEST_DATA_DIR, "jcamp", "raman_soddyite.jdx")
-    return p
-
-
-@pytest.fixture
-def raman_soddyite_scidata_jsonld_file():
-    """
-    SciData JSON-LD conversion of Soddyite RRUFF file above using SciDataLib
-    """
-    p = pathlib.Path(TEST_DATA_DIR, "scidata-jsonld", "raman_soddyite.jsonld")
-    return p
-
-
-@pytest.fixture
-def raman_soddyite_ssm_json_file():
-    """
-    SSM abbreviated JSON conversion of Soddyite RRUFF file above
-    """
-    p = pathlib.Path(TEST_DATA_DIR, "ssm-json", "raman_soddyite.json")
-    return p
 
 
 def test_convert():
     response = client.get("/convert")
     assert response.status_code == 200
     assert response.json() == {
-        "input formats": ["rruff", "jcamp", "json-ld"],
+        "input formats": ["rruff", "jcamp", "json-ld", "json"],
         "output formats": ["json", "json-ld"]
     }
 
