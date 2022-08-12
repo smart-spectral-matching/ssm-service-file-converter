127f70c39c27fcdab4dced9d5f18ea5e02f68829
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-12 15:12:38 -0400

    Simplifies app include router call

--- src/ssm_file_converter/app.py
+++ src/ssm_file_converter/app.py
@@ -4,16 +4,12 @@ from ssm_file_converter.apis import api_router
 from ssm_file_converter.config import settings
 
 
-def include_router(app):
-    app.include_router(api_router)
-
-
 def start_application():
     app = FastAPI(
         title=settings.project_name,
         version=settings.project_version
     )
-    include_router(app)
+    app.include_router(api_router)
     return app
 
 
