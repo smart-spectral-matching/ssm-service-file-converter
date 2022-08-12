7968e4dc1b631a79d3cfca9d08dee9dd1ef34d0e
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-12 16:43:29 -0400

    Adds api merge test

--- tests/apis/version1/test_merge.py
+++ tests/apis/version1/test_merge.py
@@ -1,13 +1,14 @@
 from fastapi.testclient import TestClient
 import json
+import pathlib
 
 from ssm_file_converter import app
 
-FILE_ARG = "upload_file"
+FILE_ARG = "upload_files"
 client = TestClient(app)
 
 
-def test_merge():
+def test_merge() -> None:
     response = client.get("/merge")
     assert response.status_code == 200
     assert response.json() == {
@@ -20,3 +21,23 @@ def test_merge():
         ]
     }
 
+
+def test_merge_jsonld_with_json(
+    raman_soddyite_ssm_json_file: pathlib.Path,
+    raman_soddyite_scidata_jsonld_file: pathlib.Path,
+) -> None:
+    # post ssm json file to convert to jsonld
+    with open(raman_soddyite_scidata_jsonld_file.absolute(), 'rb') as fjsonld:
+        with open(raman_soddyite_ssm_json_file.absolute(), "rb") as fjson:
+            files = [
+                (FILE_ARG, (raman_soddyite_ssm_json_file.name, fjson)),
+                (FILE_ARG, (raman_soddyite_scidata_jsonld_file.name, fjsonld)),
+            ]
+            response = client.post("/merge/jsonld", files=files)
+    assert response.status_code == 200
+    output = response.json()
+
+    with open(raman_soddyite_scidata_jsonld_file.absolute(), 'rb') as f:
+        target = json.load(f)
+
+    assert output == target
