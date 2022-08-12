6a804160cbb3820209f5a0c3ed6bf550e1278fa9
Marshall McDonnell <mcdonnellmt@ornl.gov>
2022-08-12 16:42:57 -0400

    Adds testing the system for merge service

--- tests/services/test_scidata_merger.py
+++ tests/services/test_scidata_merger.py
@@ -4,6 +4,7 @@ import tempfile
 
 from ssm_file_converter.services.scidata_merger import (
     _get_file_type,
+    _without_keys,
     get_new_data,
     get_original_data,
     merge_data_from_filenames,
@@ -38,11 +39,16 @@ def fixture_jsonld_filename(raman_soddyite_scidata_jsonld_file) -> str:
     return jsonld_filename
 
 
+def test_without_keys() -> None:
+    a = {"foo": 1, "bar": 2}
+    assert _without_keys(a, ["foo"]) == {"bar": 2}
+
+
 def test_get_file_type(jsonld_filename, json_filename) -> None:
     filenames = [jsonld_filename, json_filename]
     assert _get_file_type(filenames, file_type=".jsonld") == jsonld_filename
     assert _get_file_type(filenames, file_type=".json") == json_filename
-    
+
 
 def test_get_new_data(jsonld_filename, json_filename) -> None:
     filenames = [jsonld_filename, json_filename]
@@ -55,12 +61,12 @@ def test_get_new_data(jsonld_filename, json_filename) -> None:
 
 
 def test_get_new_data_bad_filenames() -> None:
-    filenames =[
+    filenames = [
         "a.txt",
         "b.txt",
     ]
 
-    with pytest.raises(Exception) as e:
+    with pytest.raises(Exception):
         get_new_data(filenames)
 
 
@@ -75,12 +81,12 @@ def test_get_original_data(jsonld_filename, json_filename) -> None:
 
 
 def test_get_original_data_bad_filenames() -> None:
-    filenames =[
+    filenames = [
         "a.txt",
         "b.txt",
     ]
 
-    with pytest.raises(Exception) as e:
+    with pytest.raises(Exception):
         get_original_data(filenames)
 
 
@@ -90,10 +96,13 @@ def test_merge_data_from_filenames(jsonld_filename, json_filename) -> None:
 
     assert output.get("@graph").get("title") == "NEW TITLE"
 
+    system_target = {
+        "facets": [{"@id": "", "@type": "", "bird type": "flamingo"}]
+    }
     scidata = output.get("@graph").get("scidata")
-    assert scidata.get("system") == { "facets": [{ "@id": "", "@type": "", "bird type": "flamingo"}]}
+    assert scidata.get("system") == system_target
     scidata.pop("system")
-    
+
     # Remove different title to see if rest of JSON-LD matches
     target = get_original_data(filenames)
     for f in [output, target]:
