8a9179ce5326d86c733437f41284d16974c269d3
Marshall McDonnell <mcdonnellmt@ornl.gov>
2023-08-09 16:23:25 -0400

    Adds RRUFF AUC to test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,6 +63,43 @@ def raman_studtite_ssm_json_file():
 
 
 @pytest.fixture
+def raman_auc_rruff_file():
+    """
+    Raman RRUFF file for AUC
+        https://rruff.info/tmp_rruff/Soddyite__R060361__Broad_Scan__780__0__unoriented__Raman_Data_RAW__21504.rruff  # noqa: E501
+    """
+    p = pathlib.Path(TEST_DATA_DIR, "rruff", "raman_auc.rruff")
+    return p
+
+
+@pytest.fixture
+def raman_auc_jcamp_file():
+    """
+    Raman RRUFF file for AUC (in JCAMP format)
+    """
+    p = pathlib.Path(TEST_DATA_DIR, "jcamp", "raman_auc.jdx")
+    return p
+
+
+@pytest.fixture
+def raman_auc_scidata_jsonld_file():
+    """
+    SciData JSON-LD conversion of AUC RRUFF file above using SciDataLib
+    """
+    p = pathlib.Path(TEST_DATA_DIR, "scidata-jsonld", "raman_auc.jsonld")
+    return p
+
+
+@pytest.fixture
+def raman_auc_ssm_json_file():
+    """
+    SSM abbreviated JSON conversion of AUC RRUFF file above
+    """
+    p = pathlib.Path(TEST_DATA_DIR, "ssm-json", "raman_auc.json")
+    return p
+
+
+@pytest.fixture
 def nmr_limonene_scidata_jsonld_file():
     """
     Simple SciData JSON-LD of Limonene from SSM catalog test data
