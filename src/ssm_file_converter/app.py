c19f8e02b63aeaf58d6a666ef72f1ca27c3a0970
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2024-08-14 16:11:04 -0400

    Adds cors middleware + localhost regex allowed

--- src/ssm_file_converter/app.py
+++ src/ssm_file_converter/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ssm_file_converter.apis import api_router
 from ssm_file_converter.config import settings
@@ -8,6 +9,13 @@ def start_application():
     app = FastAPI(
         title=settings.project_name,
         version=settings.project_version
+    )
+
+    origin_regex = "http*://localhost*"
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origin_regex=origin_regex,
     )
     app.include_router(api_router)
     return app
