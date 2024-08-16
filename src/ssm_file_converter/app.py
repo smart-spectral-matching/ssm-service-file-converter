cc49c3c532b8faac1b060820feabcac9ac613634
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2024-08-16 08:12:23 -0400

    Removes CORS middleware (plan for API gateway instead

--- src/ssm_file_converter/app.py
+++ src/ssm_file_converter/app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from ssm_file_converter.apis import api_router
 from ssm_file_converter.config import settings
@@ -11,18 +10,6 @@ def start_application():
         version=settings.project_version
     )
 
-    # TODO: make this general and come in from config, not hard-coded
-    origins = [
-        "http://localhost:8080",
-    ]
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
     app.include_router(api_router)
     return app
 
