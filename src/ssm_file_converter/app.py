8d21774cf74706afbf4edbf0714d97d8d3902137
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2024-08-19 13:56:00 -0400

    Adds CORS middleware back in

--- src/ssm_file_converter/app.py
+++ src/ssm_file_converter/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from ssm_file_converter.apis import api_router
 from ssm_file_converter.config import settings
@@ -8,6 +9,13 @@ def start_application():
     app = FastAPI(
         title=settings.project_name,
         version=settings.project_version
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(api_router)
